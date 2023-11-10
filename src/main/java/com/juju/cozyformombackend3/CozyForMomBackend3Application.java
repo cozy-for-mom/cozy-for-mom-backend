c32984d098f91fced49c7896d178b5207a00b176
anso33 <hyeon131812@gmail.com>
2023-11-10 17:06:46 +0900

    :tada: initial project

---
+++ src/main/java/com/juju/cozyformombackend3/CozyForMomBackend3Application.java
@@ -0,0 +1,13 @@
+package com.juju.cozyformombackend3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CozyForMomBackend3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CozyForMomBackend3Application.class, args);
+	}
+
+}
