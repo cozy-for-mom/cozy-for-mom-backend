73cbd0f108122d6ff379ce2c2f762d7de87ee0b2
anso33 <hyeon131812@gmail.com>
2024-01-29 09:09:21 +0900

    :sparkles: feature: Add get daily meal record list

--- src/main/java/com/juju/cozyformombackend3/domain/meal/repository/MealRecordRepository.java
+++ src/main/java/com/juju/cozyformombackend3/domain/meal/repository/MealRecordRepository.java
@@ -3,6 +3,6 @@ package com.juju.cozyformombackend3.domain.meal.repository;
 import com.juju.cozyformombackend3.domain.meal.model.MealRecord;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface MealRecordRepository extends JpaRepository<MealRecord, Long> {
+public interface MealRecordRepository extends JpaRepository<MealRecord, Long>, CustomMealRecordRepository {
 
 }
