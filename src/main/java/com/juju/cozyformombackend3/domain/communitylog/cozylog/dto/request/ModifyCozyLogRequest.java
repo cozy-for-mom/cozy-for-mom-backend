65fb25176794b3629ff54b6a95a02572b751179e
anso33 <hyeon131812@gmail.com>
2024-02-17 16:20:27 +0900

    fix: ImageInfo 클래스 밖으로 빼서 재사용

--- src/main/java/com/juju/cozyformombackend3/domain/communitylog/cozylog/dto/request/ModifyCozyLogRequest.java
+++ src/main/java/com/juju/cozyformombackend3/domain/communitylog/cozylog/dto/request/ModifyCozyLogRequest.java
@@ -2,6 +2,7 @@ package com.juju.cozyformombackend3.domain.communitylog.cozylog.dto.request;
 
 import java.util.List;
 
+import com.juju.cozyformombackend3.domain.communitylog.cozylog.dto.ImageInfoDto;
 import com.juju.cozyformombackend3.domain.communitylog.cozylog.model.CozyLogImage;
 import com.juju.cozyformombackend3.domain.communitylog.cozylog.model.CozyLogMode;
 
@@ -12,15 +13,8 @@ public class ModifyCozyLogRequest {
 	private Long id;
 	private String title;
 	private String content;
-	private List<Image> imageList;
+	private List<ImageInfoDto> imageList;
 	private String mode;
-
-	@Getter
-	private class Image {
-		private Long imageId;
-		private String imageUrl;
-		private String description;
-	}
 
 	public CozyLogMode getMode() {
 		return CozyLogMode.valueOf(mode);
@@ -28,13 +22,13 @@ public class ModifyCozyLogRequest {
 
 	public List<Long> getImageIdList() {
 		return imageList.stream()
-			.map(image -> image.imageId)
+			.map(image -> image.getImageId())
 			.toList();
 	}
 
 	public List<CozyLogImage> getImageList() {
 		return imageList.stream()
-			.map(image -> CozyLogImage.of(null, image.imageUrl, image.description))
+			.map(image -> CozyLogImage.of(null, image.getImageUrl(), image.getDescription()))
 			.toList();
 	}
 }
