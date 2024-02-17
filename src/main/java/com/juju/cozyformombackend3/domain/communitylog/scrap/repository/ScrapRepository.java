fed0d8f4ec5f32b1640075a540c49191a2c96a16
AN-SOHYEON <hyeon131812@gmail.com>
2024-02-18 03:17:40 +0900

    ✨ [Feature] scrap 등록, 삭제하는 API 구현 (#62)

--- src/main/java/com/juju/cozyformombackend3/domain/communitylog/scrap/repository/ScrapRepository.java
+++ src/main/java/com/juju/cozyformombackend3/domain/communitylog/scrap/repository/ScrapRepository.java
@@ -1,5 +1,7 @@
 package com.juju.cozyformombackend3.domain.communitylog.scrap.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.juju.cozyformombackend3.domain.communitylog.scrap.model.Scrap;
@@ -8,4 +10,6 @@ public interface ScrapRepository extends JpaRepository<Scrap, Long> {
 	Long countByCozyLogId(Long cozyLogId);
 
 	boolean existsByCozyLogIdAndUserUserId(Long cozyLogId, Long userId);
+
+	Optional<Scrap> findByCozyLogIdAndUserUserId(Long cozyLogId, Long userId);
 }
---
+++ src/main/java/com/juju/cozyformombackend3/domain/communitylog/scrap/service/ScrapService.java
@@ -0,0 +1,65 @@
+package com.juju.cozyformombackend3.domain.communitylog.scrap.service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.juju.cozyformombackend3.domain.communitylog.cozylog.error.CozyLogErrorCode;
+import com.juju.cozyformombackend3.domain.communitylog.cozylog.repository.CozyLogRepository;
+import com.juju.cozyformombackend3.domain.communitylog.scrap.dto.request.ApplyScrapRequest;
+import com.juju.cozyformombackend3.domain.communitylog.scrap.error.ScrapErrorCode;
+import com.juju.cozyformombackend3.domain.communitylog.scrap.model.Scrap;
+import com.juju.cozyformombackend3.domain.communitylog.scrap.repository.ScrapRepository;
+import com.juju.cozyformombackend3.domain.user.error.UserErrorCode;
+import com.juju.cozyformombackend3.domain.user.model.User;
+import com.juju.cozyformombackend3.domain.user.repository.UserRepository;
+import com.juju.cozyformombackend3.global.error.exception.BusinessException;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class ScrapService {
+	private final ScrapRepository scrapRepository;
+	private final CozyLogRepository cozyLogRepository;
+	private final UserRepository userRepository;
+
+	@Transactional
+	public void saveScrap(Long userId, ApplyScrapRequest request) {
+		if (request.getIsScraped()) {
+			addScrap(userId, request);
+		} else {
+			deleteScrap(userId, request);
+		}
+	}
+
+	private void deleteScrap(Long userId, ApplyScrapRequest request) {
+		Scrap foundScrap = scrapRepository.findByCozyLogIdAndUserUserId(request.getCozyLogId(), userId)
+			.orElseThrow(() -> new BusinessException(ScrapErrorCode.NOT_FOUND_NOT_EXIST));
+		scrapRepository.delete(foundScrap);
+	}
+
+	private void addScrap(Long userId, ApplyScrapRequest request) {
+		notExistsCozyLog(request.getCozyLogId());
+		existsScrap(userId, request.getCozyLogId());
+		User foundUser = findUserById(userId);
+		scrapRepository.save(request.toEntity(foundUser));
+	}
+
+	private void existsScrap(Long userId, Long cozyLogId) {
+		if (scrapRepository.existsByCozyLogIdAndUserUserId(cozyLogId, userId)) {
+			throw new BusinessException(ScrapErrorCode.CONFLICT_ALREADY_EXIST);
+		}
+	}
+
+	private void notExistsCozyLog(Long cozyLogId) {
+		if (!cozyLogRepository.existsById(cozyLogId)) {
+			throw new BusinessException(CozyLogErrorCode.NOT_FOUND_COZY_LOG);
+		}
+	}
+
+	private User findUserById(Long userId) {
+		return userRepository.findByUserId(userId)
+			.orElseThrow(() -> new BusinessException(UserErrorCode.NOT_FOUND_USER));
+	}
+}
