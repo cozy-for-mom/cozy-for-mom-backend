572c8df6ecd8a132ec4261aa956bd419001bc2fc
anso33 <hyeon131812@gmail.com>
2024-02-24 19:41:02 +0900

    fix: pathparameter로 recordId 받도록 수정

--- src/main/java/com/juju/cozyformombackend3/domain/userlog/bloodsugar/controller/BloodSugarController.java
+++ src/main/java/com/juju/cozyformombackend3/domain/userlog/bloodsugar/controller/BloodSugarController.java
@@ -1,5 +1,20 @@
 package com.juju.cozyformombackend3.domain.userlog.bloodsugar.controller;
 
+import java.net.URI;
+
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.juju.cozyformombackend3.domain.user.model.User;
 import com.juju.cozyformombackend3.domain.userlog.bloodsugar.dto.request.ModifyBloodSugarRecordRequest;
 import com.juju.cozyformombackend3.domain.userlog.bloodsugar.dto.request.SaveBloodSugarRecordRequest;
 import com.juju.cozyformombackend3.domain.userlog.bloodsugar.dto.response.FindBloodSugarListResponse;
@@ -7,16 +22,9 @@ import com.juju.cozyformombackend3.domain.userlog.bloodsugar.dto.response.FindDa
 import com.juju.cozyformombackend3.domain.userlog.bloodsugar.dto.response.ModifyBloodSugarRecordResponse;
 import com.juju.cozyformombackend3.domain.userlog.bloodsugar.dto.response.SaveBloodSugarRecordResponse;
 import com.juju.cozyformombackend3.domain.userlog.bloodsugar.service.BloodSugarService;
-import com.juju.cozyformombackend3.domain.user.model.User;
 import com.juju.cozyformombackend3.global.dto.response.SuccessResponse;
 
 import lombok.RequiredArgsConstructor;
-
-import org.springframework.data.domain.Pageable;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import java.net.URI;
 
 @RestController
 @RequiredArgsConstructor
@@ -36,10 +44,11 @@ public class BloodSugarController {
 	}
 
 	@PutMapping
-	public ResponseEntity<SuccessResponse> modifyBloodSugarRecord(@RequestBody ModifyBloodSugarRecordRequest request) {
+	public ResponseEntity<SuccessResponse> modifyBloodSugarRecord(@PathVariable(name = "id") Long id,
+		@RequestBody ModifyBloodSugarRecordRequest request) {
 		User user = new User();
 
-		ModifyBloodSugarRecordResponse response = bloodSugarService.updateBloodSugarRecord(request, user);
+		ModifyBloodSugarRecordResponse response = bloodSugarService.updateBloodSugarRecord(id, request, user);
 
 		return ResponseEntity.ok().body(SuccessResponse.of(200, response));
 	}
--- src/main/java/com/juju/cozyformombackend3/domain/userlog/bloodsugar/service/BloodSugarService.java
+++ src/main/java/com/juju/cozyformombackend3/domain/userlog/bloodsugar/service/BloodSugarService.java
@@ -1,5 +1,13 @@
 package com.juju.cozyformombackend3.domain.userlog.bloodsugar.service;
 
+import java.time.LocalDate;
+
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.juju.cozyformombackend3.domain.user.model.User;
 import com.juju.cozyformombackend3.domain.userlog.bloodsugar.dto.object.FindPeriodicBloodSugar;
 import com.juju.cozyformombackend3.domain.userlog.bloodsugar.dto.request.ModifyBloodSugarRecordRequest;
 import com.juju.cozyformombackend3.domain.userlog.bloodsugar.dto.request.SaveBloodSugarRecordRequest;
@@ -9,16 +17,8 @@ import com.juju.cozyformombackend3.domain.userlog.bloodsugar.dto.response.Modify
 import com.juju.cozyformombackend3.domain.userlog.bloodsugar.dto.response.SaveBloodSugarRecordResponse;
 import com.juju.cozyformombackend3.domain.userlog.bloodsugar.model.BloodSugarRecord;
 import com.juju.cozyformombackend3.domain.userlog.bloodsugar.repository.BloodSugarRepository;
-import com.juju.cozyformombackend3.domain.user.model.User;
 
 import lombok.RequiredArgsConstructor;
-
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Slice;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-
-import java.time.LocalDate;
 
 @Service
 @Transactional(readOnly = true)
@@ -34,8 +34,9 @@ public class BloodSugarService {
 	}
 
 	@Transactional
-	public ModifyBloodSugarRecordResponse updateBloodSugarRecord(ModifyBloodSugarRecordRequest request, User user) {
-		BloodSugarRecord modifiedRecord = bloodSugarRepository.findById(request.getId())
+	public ModifyBloodSugarRecordResponse updateBloodSugarRecord(Long recordId, ModifyBloodSugarRecordRequest request,
+		User user) {
+		BloodSugarRecord modifiedRecord = bloodSugarRepository.findById(recordId)
 			.orElseThrow(() -> new IllegalArgumentException("존재하지 않는 혈당 기록입니다."));
 		if (modifiedRecord.getUser() != user) {
 			throw new IllegalArgumentException("본인의 혈당 기록만 수정할 수 있습니다.");
