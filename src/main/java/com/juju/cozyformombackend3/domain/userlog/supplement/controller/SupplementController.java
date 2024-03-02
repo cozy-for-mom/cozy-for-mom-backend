61210a089b24393094ce5221c3402c1b60c2cd2e
anso33 <hyeon131812@gmail.com>
2024-03-02 22:40:06 +0900

    refactor: 나의 영양제 등록하는 API를 수정

--- src/main/java/com/juju/cozyformombackend3/domain/userlog/supplement/controller/SupplementController.java
+++ src/main/java/com/juju/cozyformombackend3/domain/userlog/supplement/controller/SupplementController.java
@@ -1,20 +1,20 @@
 package com.juju.cozyformombackend3.domain.userlog.supplement.controller;
 
-import com.juju.cozyformombackend3.domain.userlog.supplement.dto.response.RegisterSupplementResponse;
-import com.juju.cozyformombackend3.domain.userlog.supplement.dto.request.RegisterSupplementRequest;
-import com.juju.cozyformombackend3.domain.userlog.supplement.service.SupplementService;
-import com.juju.cozyformombackend3.domain.user.model.User;
-import com.juju.cozyformombackend3.global.dto.response.CommonResponse;
-
 import java.net.URI;
-
-import lombok.RequiredArgsConstructor;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.juju.cozyformombackend3.domain.userlog.supplement.dto.request.RegisterSupplementRequest;
+import com.juju.cozyformombackend3.domain.userlog.supplement.dto.response.RegisterSupplementResponse;
+import com.juju.cozyformombackend3.domain.userlog.supplement.service.SupplementService;
+import com.juju.cozyformombackend3.global.auth.annotation.LoginUserId;
+import com.juju.cozyformombackend3.global.dto.response.CommonResponse;
+
+import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 @RestController
@@ -24,12 +24,13 @@ public class SupplementController {
 	private final SupplementService supplementService;
 
 	@PostMapping("")
-	public ResponseEntity<CommonResponse> registerSupplement(@RequestBody RegisterSupplementRequest request) {
-		Long userId = 1L;
-		User user = new User();
-
+	public ResponseEntity<CommonResponse> registerSupplement(
+		@LoginUserId Long userId,
+		@RequestBody RegisterSupplementRequest request) {
+		RegisterSupplementResponse registerSupplementResponse = supplementService.registerSupplement(userId, request);
+		//TODO: 유의미한 userID로 변경
 		URI uri = URI.create("/api/v1/supplement/" + userId);
-		RegisterSupplementResponse registerSupplementResponse = supplementService.registerSupplement(request, user);
+
 		return ResponseEntity.created(uri).body(CommonResponse.of(registerSupplementResponse));
 	}
 }
--- src/main/java/com/juju/cozyformombackend3/domain/userlog/supplement/service/SupplementService.java
+++ src/main/java/com/juju/cozyformombackend3/domain/userlog/supplement/service/SupplementService.java
@@ -1,14 +1,15 @@
 package com.juju.cozyformombackend3.domain.userlog.supplement.service;
 
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.juju.cozyformombackend3.domain.user.model.User;
+import com.juju.cozyformombackend3.domain.user.repository.UserRepository;
 import com.juju.cozyformombackend3.domain.userlog.supplement.dto.request.RegisterSupplementRequest;
 import com.juju.cozyformombackend3.domain.userlog.supplement.dto.response.RegisterSupplementResponse;
 import com.juju.cozyformombackend3.domain.userlog.supplement.repository.SupplementRepository;
-import com.juju.cozyformombackend3.domain.user.model.User;
 
 import lombok.RequiredArgsConstructor;
-
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @Transactional(readOnly = true)
@@ -16,14 +17,21 @@ import org.springframework.transaction.annotation.Transactional;
 public class SupplementService {
 
 	private final SupplementRepository supplementRepository;
+	private final UserRepository userRepository;
 
 	@Transactional
-	public RegisterSupplementResponse registerSupplement(RegisterSupplementRequest request, User user) {
+	public RegisterSupplementResponse registerSupplement(Long userId, RegisterSupplementRequest request) {
+		User user = findByUserId(userId);
 		if (supplementRepository.existsBySupplementName(request.getSupplementName())) {
 			throw new IllegalArgumentException("이미 존재하는 보충제 이름입니다.");
 		} else {
 			return RegisterSupplementResponse.of(
 				user.registerSupplement(request.getSupplementName(), request.getTargetCount()));
 		}
+	}
+
+	private User findByUserId(Long userId) {
+		return userRepository.findByUserId(userId)
+			.orElseThrow(() -> new IllegalArgumentException("존재하지 않는 사용자입니다."));
 	}
 }
