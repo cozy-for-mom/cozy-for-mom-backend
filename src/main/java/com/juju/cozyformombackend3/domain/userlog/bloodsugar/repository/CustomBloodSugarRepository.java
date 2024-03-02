8e54e7b72b744c1c6942ebc8c68e1a7260f6082c
anso33 <hyeon131812@gmail.com>
2024-03-02 14:05:05 +0900

    remove: Delete useless method

--- src/main/java/com/juju/cozyformombackend3/domain/userlog/bloodsugar/repository/CustomBloodSugarRepository.java
+++ src/main/java/com/juju/cozyformombackend3/domain/userlog/bloodsugar/repository/CustomBloodSugarRepository.java
@@ -1,10 +1,6 @@
 package com.juju.cozyformombackend3.domain.userlog.bloodsugar.repository;
 
-import java.time.LocalDate;
 import java.util.List;
-
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Slice;
 
 import com.juju.cozyformombackend3.domain.userlog.bloodsugar.dto.object.FindDaliyBloodSugar;
 import com.juju.cozyformombackend3.domain.userlog.bloodsugar.dto.object.FindPeriodBloodSugarCondition;
@@ -12,12 +8,6 @@ import com.juju.cozyformombackend3.domain.userlog.bloodsugar.dto.object.FindPeri
 
 public interface CustomBloodSugarRepository {
 	List<FindDaliyBloodSugar> searchAllByCreatedAt(Long userId, String createdAt);
-
-	Slice<FindPeriodicBloodSugar> searchAllByPeriodType(long userId, String date, String type, Pageable pageable);
-
-	Slice<FindPeriodicBloodSugar> searchAllByDailyType(long userId, LocalDate date, Pageable pageable);
-
-	Slice<FindPeriodicBloodSugar> searchAllByWeeklyType(long userId, LocalDate date, Pageable pageable);
 
 	List<FindPeriodicBloodSugar> findPeriodRecordByDate(FindPeriodBloodSugarCondition condition);
 }
