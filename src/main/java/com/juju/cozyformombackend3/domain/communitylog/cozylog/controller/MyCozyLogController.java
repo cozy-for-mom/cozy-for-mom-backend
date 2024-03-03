249a2947206d8d9f7eea67c0df2682bb81d2c10e
anso33 <hyeon131812@gmail.com>
2024-03-03 23:15:52 +0900

    feature: 내가 쓴 코지로그 bulk 삭제

--- src/main/java/com/juju/cozyformombackend3/domain/communitylog/cozylog/controller/MyCozyLogController.java
+++ src/main/java/com/juju/cozyformombackend3/domain/communitylog/cozylog/controller/MyCozyLogController.java
@@ -2,10 +2,13 @@ package com.juju.cozyformombackend3.domain.communitylog.cozylog.controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.juju.cozyformombackend3.domain.communitylog.cozylog.dto.request.DeleteMyCozyLogListRequest;
 import com.juju.cozyformombackend3.domain.communitylog.cozylog.dto.response.FindMyCozyLogListResponse;
 import com.juju.cozyformombackend3.domain.communitylog.cozylog.service.CozyLogService;
 import com.juju.cozyformombackend3.global.auth.annotation.LoginUserId;
@@ -27,5 +30,14 @@ public class MyCozyLogController {
 		FindMyCozyLogListResponse response = cozyLogService.findMyCozyLog(userId, reportId, size);
 
 		return ResponseEntity.ok().body(SuccessResponse.of(200, response));
+	}
+
+	@PostMapping
+	public ResponseEntity<SuccessResponse> removeMyCozyLogList(
+		@LoginUserId Long userId,
+		@RequestBody DeleteMyCozyLogListRequest request) {
+		cozyLogService.deleteCozyLogList(userId, request);
+
+		return ResponseEntity.ok().body(SuccessResponse.of(200, null));
 	}
 }
