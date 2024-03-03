e9e747ef4ea4b29764b14db73eeaf13fd8f933d0
anso33 <hyeon131812@gmail.com>
2024-03-03 11:09:58 +0900

    feature: 내 정보 조회 API 구현

--- src/main/java/com/juju/cozyformombackend3/domain/user/repository/UserRepository.java
+++ src/main/java/com/juju/cozyformombackend3/domain/user/repository/UserRepository.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Repository;
 import com.juju.cozyformombackend3.domain.user.model.User;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, Long>, CustomUserRepository {
 
 	Optional<User> findByEmail(String email);
 
--- src/main/java/com/juju/cozyformombackend3/global/repository/DateParser.java
+++ src/main/java/com/juju/cozyformombackend3/global/repository/DateParser.java
@@ -1,7 +1,11 @@
 package com.juju.cozyformombackend3.global.repository;
 
+import java.text.SimpleDateFormat;
 import java.time.LocalDateTime;
+import java.util.Date;
 
+import com.querydsl.core.types.Expression;
+import com.querydsl.core.types.dsl.DatePath;
 import com.querydsl.core.types.dsl.DateTimeExpression;
 import com.querydsl.core.types.dsl.DateTimePath;
 import com.querydsl.core.types.dsl.Expressions;
@@ -14,5 +18,11 @@ public class DateParser {
 
 	public static StringExpression getDateFromDateTime(DateTimeExpression<LocalDateTime> dateTime) {
 		return Expressions.stringTemplate("FUNCTION('DATE_FORMAT', {0}, '%Y-%m-%d')", dateTime);
+	}
+
+	public static Expression<String> getStringFromDatePath(DatePath<Date> datePath) {
+		String formatPattern = "yyyy-MM-dd";
+
+		return datePath.stringValue().concat(new SimpleDateFormat(formatPattern).format(datePath));
 	}
 }
