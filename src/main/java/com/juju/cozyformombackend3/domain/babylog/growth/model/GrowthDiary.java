115a85482db8e785eadc382cd25941b0153b771e
anso33 <hyeon131812@gmail.com>
2024-03-09 15:43:54 +0900

    fix: delete useless column

--- src/main/java/com/juju/cozyformombackend3/domain/babylog/growth/model/GrowthDiary.java
+++ src/main/java/com/juju/cozyformombackend3/domain/babylog/growth/model/GrowthDiary.java
@@ -2,14 +2,17 @@ package com.juju.cozyformombackend3.domain.babylog.growth.model;
 
 import java.time.LocalDate;
 
+import com.juju.cozyformombackend3.domain.babylog.baby.model.BabyProfile;
 import com.juju.cozyformombackend3.domain.babylog.growth.dto.request.UpdateGrowthRequest;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import lombok.AccessLevel;
@@ -22,54 +25,47 @@ import lombok.NoArgsConstructor;
 @Getter
 public class GrowthDiary {
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	@Column(name = "id")
-	private Long id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
 
-	// @ManyToOne(fetch = FetchType.LAZY)
-	// @JoinColumn(name = "baby_profile_id")
-	// private BabyProfile babyProfile;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "baby_profile_id")
+    private BabyProfile babyProfile;
 
-	@Column(name = "record_at", nullable = false)
-	private LocalDate recordAt;
+    @Column(name = "image_url", columnDefinition = "TEXT")
+    private String imageUrl;
 
-	@Column(name = "image_url", columnDefinition = "TEXT")
-	private String imageUrl;
+    @Column(name = "title", columnDefinition = "TEXT")
+    private String title;
 
-	@Column(name = "title", columnDefinition = "TEXT")
-	private String title;
+    @Column(name = "content", columnDefinition = "TEXT")
+    private String content;
 
-	@Column(name = "content", columnDefinition = "TEXT")
-	private String content;
+    @OneToOne
+    private GrowthReport growthReport;
 
-	@OneToOne
-	@JoinColumn(name = "growth_report_id")
-	private GrowthReport growthReport;
+    private GrowthDiary(BabyProfile babyProfile, LocalDate recordAt, String growthImageUrl, String title,
+        String content) {
+        this.babyProfile = babyProfile;
+        this.imageUrl = growthImageUrl;
+        this.title = title;
+        this.content = content;
+    }
 
-	private GrowthDiary(LocalDate recordAt, String growthImageUrl, String title,
-		String content, GrowthReport growthReport) {
-		// this.babyProfile = babyProfile;
-		this.recordAt = recordAt;
-		this.imageUrl = growthImageUrl;
-		this.title = title;
-		this.content = content;
-		this.growthReport = growthReport;
-	}
+    public static GrowthDiary of(BabyProfile babyProfile, LocalDate recordAt, String growthImageUrl, String title,
+        String content) {
+        return new GrowthDiary(babyProfile, recordAt, growthImageUrl, title, content);
+    }
 
-	public static GrowthDiary of(LocalDate recordAt, String growthImageUrl, String title,
-		String content) {
-		return new GrowthDiary(recordAt, growthImageUrl, title, content, null);
-	}
+    public void update(UpdateGrowthRequest.GrowthDiaryDto growthDiaryDto) {
+        this.imageUrl = growthDiaryDto.getGrowthImageUrl();
+        this.title = growthDiaryDto.getTitle();
+        this.content = growthDiaryDto.getContent();
+    }
 
-	public void update(UpdateGrowthRequest.GrowthDiaryDto growthDiaryDto) {
-		this.recordAt = growthDiaryDto.getDate();
-		this.imageUrl = growthDiaryDto.getGrowthImageUrl();
-		this.title = growthDiaryDto.getTitle();
-		this.content = growthDiaryDto.getContent();
-	}
-
-	public void updateGrowthReport(GrowthReport growthReport) {
-		this.growthReport = growthReport;
-	}
+    public void updateGrowthReport(GrowthReport growthReport) {
+        this.growthReport = growthReport;
+    }
 }
