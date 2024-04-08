7b4c613b2d59ca25214accc8bed210e27cd6eff0
AN-SOHYEON <hyeon131812@gmail.com>
2024-04-08 19:05:06 +0900

    ✨ [Feature] 회원가입시 필요한 중복 확인 API 구현 (#153)

--- src/main/java/com/juju/cozyformombackend3/global/auth/controller/AuthController.java
+++ src/main/java/com/juju/cozyformombackend3/global/auth/controller/AuthController.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.juju.cozyformombackend3.global.auth.dto.CheckNicknameDto;
+import com.juju.cozyformombackend3.global.auth.dto.CheckOAuthAccountDto;
 import com.juju.cozyformombackend3.global.auth.dto.api.SignInDto;
 import com.juju.cozyformombackend3.global.auth.filter.JwtFilter;
 import com.juju.cozyformombackend3.global.auth.service.AuthService;
@@ -17,12 +19,12 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
 @RestController
-@RequestMapping("/api/v1/")
+@RequestMapping("/api/v1/authenticate")
 @RequiredArgsConstructor
 public class AuthController {
     private final AuthService authService;
 
-    @PostMapping("/authenticate")
+    @PostMapping
     public ResponseEntity<SuccessResponse> signIn(@Valid @RequestBody SignInDto.Request request) {
         String accessToken = authService.authenticate(request);
 
@@ -30,5 +32,21 @@ public class AuthController {
         httpHeaders.add(JwtFilter.AUTHORIZATION_HEADER, "Bearer " + accessToken);
 
         return new ResponseEntity<>(SuccessResponse.of(HttpStatus.OK.value(), null), httpHeaders, HttpStatus.OK);
+    }
+
+    @PostMapping("/nickname")
+    public ResponseEntity<SuccessResponse> checkDuplicateNickname(
+        @Valid @RequestBody CheckNicknameDto.Request request) {
+        CheckNicknameDto.Response response = authService.checkExistsNickname(request);
+
+        return ResponseEntity.ok().body(SuccessResponse.of(HttpStatus.OK.value(), response));
+    }
+
+    @PostMapping("/oauth")
+    public ResponseEntity<SuccessResponse> checkDuplicateOAuthAccount(
+        @Valid @RequestBody CheckOAuthAccountDto.Request request) {
+        CheckOAuthAccountDto.Response response = authService.checkExistsOAuthAccount(request);
+
+        return ResponseEntity.ok().body(SuccessResponse.of(HttpStatus.OK.value(), response));
     }
 }
--- src/main/java/com/juju/cozyformombackend3/global/auth/service/AuthService.java
+++ src/main/java/com/juju/cozyformombackend3/global/auth/service/AuthService.java
@@ -6,6 +6,8 @@ import org.springframework.transaction.annotation.Transactional;
 
 import com.juju.cozyformombackend3.domain.user.model.User;
 import com.juju.cozyformombackend3.domain.user.repository.UserRepository;
+import com.juju.cozyformombackend3.global.auth.dto.CheckNicknameDto;
+import com.juju.cozyformombackend3.global.auth.dto.CheckOAuthAccountDto;
 import com.juju.cozyformombackend3.global.auth.dto.api.SignInDto;
 import com.juju.cozyformombackend3.global.auth.error.AuthErrorCode;
 import com.juju.cozyformombackend3.global.auth.service.token.TokenProvider;
@@ -38,5 +40,23 @@ public class AuthService {
         log.info("herhe before generateToken");
 
         return tokenProvider.generateUserToken(findUser);
+    }
+
+    public CheckNicknameDto.Response checkExistsNickname(CheckNicknameDto.Request request) {
+        if (userRepository.existsByNickname(request.getNickname())) {
+            throw new BusinessException(AuthErrorCode.CONFLICT_EXIST_NICKNAME);
+        } else {
+            return CheckNicknameDto.Response.of(request.getNickname());
+        }
+    }
+
+    public CheckOAuthAccountDto.Response checkExistsOAuthAccount(CheckOAuthAccountDto.Request request) {
+        if (userRepository.existsByEmail(request.getEmail())) {
+            throw new BusinessException(AuthErrorCode.CONFLICT_EXIST_EMAIL);
+        }
+        if (userRepository.existsByOauthIdAndOauth2Registration(request.getOauthId(), request.getOauthType())) {
+            throw new BusinessException(AuthErrorCode.CONFLICT_EXIST_OAUTH_ACCOUNT);
+        }
+        return CheckOAuthAccountDto.Response.of(request);
     }
 }
