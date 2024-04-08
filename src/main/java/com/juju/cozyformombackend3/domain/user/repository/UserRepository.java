dbad8b831fe65a039187e29ce33dbabef2e4c1fb
anso33 <hyeon131812@gmail.com>
2024-04-08 17:38:47 +0900

    feature: Add signIn API

--- src/main/java/com/juju/cozyformombackend3/domain/user/repository/UserRepository.java
+++ src/main/java/com/juju/cozyformombackend3/domain/user/repository/UserRepository.java
@@ -1,12 +1,17 @@
 package com.juju.cozyformombackend3.domain.user.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.juju.cozyformombackend3.domain.user.model.User;
+import com.juju.cozyformombackend3.global.auth.model.OAuth2Registration;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long>, CustomUserRepository {
 
     User findByEmail(String email);
+
+    Optional<User> findByOauthIdAndOauth2Registration(String oauthId, OAuth2Registration oAuthType);
 }
--- src/main/java/com/juju/cozyformombackend3/global/auth/error/AuthErrorCode.java
+++ src/main/java/com/juju/cozyformombackend3/global/auth/error/AuthErrorCode.java
@@ -9,7 +9,8 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public enum AuthErrorCode implements ErrorCode {
     NOT_FOUND_NOT_MATCH_OAUTH2(HttpStatus.NOT_FOUND.value(), "지원하지 않는 소셜 로그인입니다."),
-    UNAUTHORIZED_OAUTH_RETURN_NULL_EMAIL(HttpStatus.UNAUTHORIZED.value(), "소셜 서비스에서 사용자의 이메일 정보를 찾을 수 없습니다.");
+    UNAUTHORIZED_OAUTH_RETURN_NULL_EMAIL(HttpStatus.UNAUTHORIZED.value(), "소셜 서비스에서 사용자의 이메일 정보를 찾을 수 없습니다."),
+    NOT_FOUND_USER(HttpStatus.NOT_FOUND.value(), "존재하지 않는 회원입니다.");
 
     private final int status;
     private final String message;
