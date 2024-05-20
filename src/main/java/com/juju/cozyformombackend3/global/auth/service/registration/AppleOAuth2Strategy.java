47bdeb8dff3f9d4d05b2ceda18976a3210613c25
anso33 <hyeon131812@gmail.com>
2024-05-20 19:36:59 +0900

    feature: Add apple unlink account method

--- src/main/java/com/juju/cozyformombackend3/global/auth/service/registration/AppleOAuth2Strategy.java
+++ src/main/java/com/juju/cozyformombackend3/global/auth/service/registration/AppleOAuth2Strategy.java
@@ -1,13 +1,17 @@
 package com.juju.cozyformombackend3.global.auth.service.registration;
 
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 
+import com.juju.cozyformombackend3.global.auth.error.AuthErrorCode;
 import com.juju.cozyformombackend3.global.auth.model.OAuth2Registration;
 import com.juju.cozyformombackend3.global.auth.model.OAuth2UserInfo;
 import com.juju.cozyformombackend3.global.auth.service.registration.client.AppleApiClient;
 import com.juju.cozyformombackend3.global.auth.service.token.AppleTokenProvider;
+import com.juju.cozyformombackend3.global.error.exception.AuthException;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -65,6 +69,21 @@ public class AppleOAuth2Strategy implements OAuth2Strategy {
 
     @Override
     public boolean unlinkOAuth2Account(String oauthValue) {
-        return false;
+
+        final MultiValueMap<String, String> requestBody = new LinkedMultiValueMap<>();
+        requestBody.add("client_id", appleAuthProperties.getClientId());
+        requestBody.add("client_secret", appleTokenProvider.generateClientSecret());
+        requestBody.add("token", oauthValue);
+
+        log.info("Apple account unlink request {}", requestBody);
+        final ResponseEntity response = appleApiClient.unlinkAccount(
+            MediaType.APPLICATION_FORM_URLENCODED_VALUE, requestBody);
+        if (response.getStatusCode().is2xxSuccessful()) {
+            log.info("Kakao account unlink success response {}", response.getBody());
+            return true;
+        } else {
+            log.error("Kakao account unlink failed response {}", response.getBody());
+            throw new AuthException(AuthErrorCode.SERVER_ERROR_UNLINK_FAILED);
+        }
     }
 }
