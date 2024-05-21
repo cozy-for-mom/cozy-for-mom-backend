f421f89a2d93f67f4996e5ac9175f2b631fc8c18
AN-SOHYEON <hyeon131812@gmail.com>
2024-05-21 13:52:25 +0900

    ✨ [Feature] apple 회원탈퇴 API 구현 (#202)
    feature: Add apple unlink account method

--- src/main/java/com/juju/cozyformombackend3/global/auth/service/registration/client/AppleApiClient.java
+++ src/main/java/com/juju/cozyformombackend3/global/auth/service/registration/client/AppleApiClient.java
@@ -1,6 +1,7 @@
 package com.juju.cozyformombackend3.global.auth.service.registration.client;
 
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
@@ -16,6 +17,11 @@ public interface AppleApiClient {
 
     @PostExchange("https://appleid.apple.com/auth/token")
     AppleToken.Response getToken(
+        @RequestHeader(HttpHeaders.CONTENT_TYPE) String contentType,
+        @RequestBody MultiValueMap request);
+
+    @PostExchange("https://appleid.apple.com/auth/revoke")
+    ResponseEntity unlinkAccount(
         @RequestHeader(HttpHeaders.CONTENT_TYPE) String contentType,
         @RequestBody MultiValueMap request);
 
