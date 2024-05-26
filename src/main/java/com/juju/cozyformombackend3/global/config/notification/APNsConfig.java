b4d06f3be619bfcbd89cf2bf53e390c1a631d426
anso33 <hyeon131812@gmail.com>
2024-05-26 16:22:56 +0900

    feature: Set Apns basic config

---
+++ src/main/java/com/juju/cozyformombackend3/global/config/notification/APNsConfig.java
@@ -0,0 +1,33 @@
+package com.juju.cozyformombackend3.global.config.notification;
+
+import java.io.IOException;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+
+import com.eatthepath.pushy.apns.ApnsClient;
+import com.eatthepath.pushy.apns.ApnsClientBuilder;
+
+import lombok.RequiredArgsConstructor;
+
+@Configuration
+@RequiredArgsConstructor
+public class APNsConfig {
+    private final APNsProperties apnsProperties;
+
+    @Bean
+    public ApnsClient someAppDevApnsClient() {
+
+        try {
+            return new ApnsClientBuilder()
+                .setApnsServer(ApnsClientBuilder.DEVELOPMENT_APNS_HOST)
+                // .setClientCredentials(new File("/path/to/certificate.p12"), "p12-file-password")
+                .setClientCredentials(new ClassPathResource(apnsProperties.getCertPath()).getInputStream(),
+                    apnsProperties.getCertPassword())
+                .build();
+        } catch (IOException e) {
+            throw new RuntimeException("apns 인증서 getInputStream 실패");
+        }
+    }
+}
