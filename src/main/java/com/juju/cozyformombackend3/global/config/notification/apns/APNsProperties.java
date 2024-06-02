bb8e1a3a139ffeab171d172469eac3445665190d
AN-SOHYEON <hyeon131812@gmail.com>
2024-06-02 14:53:05 +0900

    ✨ [Feature] 알림 전송 기능 구현하기 (#206)
    * feature: Set Apns basic config
    
    * feature: APNS랑 TLS 인증까지 성공
    
    * feature: Add config to execute async task
    
    * feature: push noti test api
    
    * chore: add log

---
+++ src/main/java/com/juju/cozyformombackend3/global/config/notification/apns/APNsProperties.java
@@ -0,0 +1,15 @@
+package com.juju.cozyformombackend3.global.config.notification.apns;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@ConfigurationProperties(prefix = "notification.apns")
+@Getter
+@RequiredArgsConstructor
+public class APNsProperties {
+    private final String clientId;
+    private final String certPath;
+    private final String certPassword;
+}
