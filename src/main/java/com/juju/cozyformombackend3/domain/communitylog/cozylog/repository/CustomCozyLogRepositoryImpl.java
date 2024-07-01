16e52084bbef2c3176a30a0d4a661f7e500d13ac
anso33 <hyeon131812@gmail.com>
2024-07-01 17:14:54 +0900

    fix: 검색 결과가 없으면 빈 리스트 가도록

--- src/main/java/com/juju/cozyformombackend3/domain/communitylog/cozylog/repository/CustomCozyLogRepositoryImpl.java
+++ src/main/java/com/juju/cozyformombackend3/domain/communitylog/cozylog/repository/CustomCozyLogRepositoryImpl.java
@@ -1,14 +1,5 @@
 package com.juju.cozyformombackend3.domain.communitylog.cozylog.repository;
 
-import static com.juju.cozyformombackend3.domain.communitylog.comment.model.QComment.*;
-import static com.juju.cozyformombackend3.domain.communitylog.cozylog.model.QCozyLog.*;
-import static com.juju.cozyformombackend3.domain.communitylog.cozylog.model.QCozyLogImage.*;
-import static com.juju.cozyformombackend3.domain.communitylog.scrap.model.QScrap.*;
-import static com.querydsl.core.types.dsl.Expressions.*;
-
-import java.util.List;
-import java.util.Objects;
-
 import com.juju.cozyformombackend3.domain.communitylog.cozylog.controller.CozyLogCondition;
 import com.juju.cozyformombackend3.domain.communitylog.cozylog.dto.querydto.CozyLogSummary;
 import com.juju.cozyformombackend3.domain.communitylog.cozylog.dto.querydto.QCozyLogSummary;
@@ -20,9 +11,17 @@ import com.querydsl.core.types.SubQueryExpression;
 import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.JPAExpressions;
 import com.querydsl.jpa.impl.JPAQueryFactory;
-
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+
+import java.util.List;
+import java.util.Objects;
+
+import static com.juju.cozyformombackend3.domain.communitylog.comment.model.QComment.comment;
+import static com.juju.cozyformombackend3.domain.communitylog.cozylog.model.QCozyLog.cozyLog;
+import static com.juju.cozyformombackend3.domain.communitylog.cozylog.model.QCozyLogImage.cozyLogImage;
+import static com.juju.cozyformombackend3.domain.communitylog.scrap.model.QScrap.scrap;
+import static com.querydsl.core.types.dsl.Expressions.numberTemplate;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -32,63 +31,63 @@ public class CustomCozyLogRepositoryImpl implements CustomCozyLogRepository {
     @Override
     public List<CozyLogSummary> searchCozyLogListByCondition(CozyLogCondition condition) {
         SubQueryExpression<Long> firstImageSubQuery = JPAExpressions
-            .select(cozyLogImage.id.min())
-            .from(cozyLogImage)
-            .where(cozyLogImage.cozyLog.id.eq(cozyLog.id));
+                .select(cozyLogImage.id.min())
+                .from(cozyLogImage)
+                .where(cozyLogImage.cozyLog.id.eq(cozyLog.id));
 
         return jpaQueryFactory.select(new QCozyLogSummary(
-                cozyLog.id, cozyLog.title, cozyLog.content.substring(0, 40),
-                cozyLog.createdAt, cozyLog.mode,
-                comment.id.count(), scrap.id.count(),
-                cozyLogImage.imageUrl.coalesce(""), cozyLog.cozyLogImageList.size()))
-            .from(cozyLog)
-            .leftJoin(comment).on(comment.cozyLog.id.eq(cozyLog.id))
-            .leftJoin(scrap).on(scrap.cozyLogId.eq(cozyLog.id))
-            .leftJoin(cozyLogImage).on(cozyLogImage.id.eq(firstImageSubQuery))
-            .where(allowPrivateModeByUserId(condition.getUserId())
-                .and(filterLogByWriterId(condition.getWriterId()))
-                .and(searchByKeyword(condition.getKeyword()))
-                .and(ltReportId(condition.getLastLogId())))
-            .groupBy(cozyLog.id, cozyLog.title, cozyLog.content, cozyLog.createdAt, cozyLog.mode,
-                cozyLogImage.imageUrl)
-            .orderBy(createOrderSpecifier(condition.getSort()), cozyLog.id.desc())
-            .limit(condition.getSize())
-            .fetch();
+                        cozyLog.id, cozyLog.title, cozyLog.content.substring(0, 40),
+                        cozyLog.createdAt, cozyLog.mode,
+                        comment.id.count(), scrap.id.count(),
+                        cozyLogImage.imageUrl.coalesce(""), cozyLog.cozyLogImageList.size()))
+                .from(cozyLog)
+                .leftJoin(comment).on(comment.cozyLog.id.eq(cozyLog.id))
+                .leftJoin(scrap).on(scrap.cozyLogId.eq(cozyLog.id))
+                .leftJoin(cozyLogImage).on(cozyLogImage.id.eq(firstImageSubQuery))
+                .where(searchByKeyword(condition.getKeyword())
+                        .and(filterLogByWriterId(condition.getWriterId()))
+                        .and(allowPrivateModeByUserId(condition.getUserId()))
+                        .and(ltReportId(condition.getLastLogId())))
+                .groupBy(cozyLog.id, cozyLog.title, cozyLog.content, cozyLog.createdAt, cozyLog.mode,
+                        cozyLogImage.imageUrl)
+                .orderBy(createOrderSpecifier(condition.getSort()), cozyLog.id.desc())
+                .limit(condition.getSize())
+                .fetch();
     }
 
     @Override
     public void deleteCozyLogByUserIdAndCozyLogIds(Long userId, List<Long> cozyLogIds) {
         jpaQueryFactory.delete(comment)
-            .where(comment.cozyLog.id.in(cozyLogIds))
-            .execute();
+                .where(comment.cozyLog.id.in(cozyLogIds))
+                .execute();
 
         jpaQueryFactory.delete(cozyLogImage)
-            .where(cozyLogImage.cozyLog.id.in(cozyLogIds))
-            .execute();
+                .where(cozyLogImage.cozyLog.id.in(cozyLogIds))
+                .execute();
 
         jpaQueryFactory.delete(scrap)
-            .where(scrap.cozyLogId.in(cozyLogIds))
-            .execute();
+                .where(scrap.cozyLogId.in(cozyLogIds))
+                .execute();
 
         jpaQueryFactory.delete(cozyLog)
-            .where(cozyLog.user.id.eq(userId).and(cozyLog.id.in(cozyLogIds)))
-            .execute();
+                .where(cozyLog.user.id.eq(userId).and(cozyLog.id.in(cozyLogIds)))
+                .execute();
     }
 
     @Override
     public Long countByCondition(CozyLogCondition condition) {
         return jpaQueryFactory.select(cozyLog.count())
-            .from(cozyLog)
-            .where(allowPrivateModeByUserId(condition.getUserId())
-                .and(filterLogByWriterId(condition.getWriterId()))
-                .and(searchByKeyword(condition.getKeyword())))
-            .fetchOne();
+                .from(cozyLog)
+                .where(allowPrivateModeByUserId(condition.getUserId())
+                        .and(filterLogByWriterId(condition.getWriterId()))
+                        .and(searchByKeyword(condition.getKeyword())))
+                .fetchOne();
     }
 
     private BooleanExpression filterLogByWriterId(Long writerId) {
         return Objects.nonNull(writerId) ?
-            cozyLog.user.id.eq(writerId)
-            : null;
+                cozyLog.user.id.eq(writerId)
+                : null;
     }
 
     private BooleanExpression searchByKeyword(String keyword) {
@@ -98,16 +97,16 @@ public class CustomCozyLogRepositoryImpl implements CustomCozyLogRepository {
         final String formattedSearchWord = "\"+" + keyword + "*\"";
 
         return (numberTemplate(Double.class, "function('match_against', {0}, {1})",
-            cozyLog.title, formattedSearchWord)
-            .gt(0))
-            .or(numberTemplate(Double.class, "function('match_against', {0}, {1})",
-                cozyLog.content, formattedSearchWord)
-                .gt(0)); //MATCH_THRESHOLD
+                cozyLog.title, formattedSearchWord)
+                .gt(0))
+                .or(numberTemplate(Double.class, "function('match_against', {0}, {1})",
+                        cozyLog.content, formattedSearchWord)
+                        .gt(0)); //MATCH_THRESHOLD
     }
 
     private BooleanExpression allowPrivateModeByUserId(Long userId) {
         return cozyLog.mode.eq(CozyLogMode.PUBLIC)
-            .or(cozyLog.mode.eq(CozyLogMode.PRIVATE).and(cozyLog.user.id.eq(userId)));
+                .or(cozyLog.mode.eq(CozyLogMode.PRIVATE).and(cozyLog.user.id.eq(userId)));
     }
 
     private BooleanExpression ltReportId(Long reportId) {
